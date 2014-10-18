8ce7ae95a204acfcbab690cd4108bf63f6591492
Gabriel Ceravolo <gcerav@gmail.com>
2014-10-18 06:48:38 -0300

    Hola Mundo
    Ejemplo cásico

---
+++ 01-Hola Mundo/HolaMundo.hx
@@ -0,0 +1,18 @@
+/*
+	Primer Programa en Haxe
+        
+	para compilarlo:
+
+	$> Haxe -main HolaMundo -Neko Hola.n
+
+	para ejecutarlo
+
+	$> Neko Hola.n
+
+*/
+
+class HolaMundo {
+  static public function main():Void {
+    trace("Hola Mundo!");
+  }
+}
